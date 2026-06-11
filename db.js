a45ab6e434153b057a8ef08fff065f34157b6c69
DipakGupta2006 <idipakgupta2006@gmail.com>
2026-06-11 18:39:42 +0530

    Added start script for Render deployment

--- db.js
+++ db.js
@@ -1,11 +1,13 @@
 const mysql = require("mysql2");
+
 const pool = mysql.createPool({ 
   host: process.env.DB_HOST, 
   user: process.env.DB_USER, 
   password: process.env.DB_PASSWORD, 
   database: process.env.DB_NAME, 
   waitForConnections: true, 
-  connectionLimit: 10 
+  connectionLimit: 10, // <-- Yahan comma lagana zaroori tha
+  ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool.promise();
