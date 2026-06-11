4a654b8830be3bbc08a64c180adfc88e91dfc9f7
DipakGupta2006 <idipakgupta2006@gmail.com>
2026-06-11 17:10:35 +0530

    database updated Completed

--- database/schema.sql
+++ database/schema.sql
@@ -15,4 +15,32 @@ SELECT
 FROM
     users;
 
-    
+CREATE TABLE
+    passwords (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        app_name VARCHAR(255) NOT NULL,
+        category ENUM (
+            'Website',
+            'Application',
+            'Social_Media',
+            'Banking',
+            'Work',
+            'Other'
+        ) NOT NULL,
+        username VARCHAR(255) NOT NULL,
+        password VARCHAR(255) NOT NULL,
+        notes TEXT,
+        favorite BOOLEAN DEFAULT FALSE,
+        created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+        updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
+    );
+
+ALTER TABLE passwords
+ADD COLUMN deleted_at TIMESTAMP NULL DEFAULT NULL;
+
+DESC passwords;
+
+SELECT
+    *
+FROM
+    passwords;
